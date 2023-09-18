ec5528e9c9cb6bc47f6de7c326180cb4d103dcd2
Colin LeFevre <colin.lefevre.school@gmail.com>
2023-09-18 16:18:54 -0400

    class work

--- App.js
+++ App.js
@@ -1,20 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Button } from "react-native";
+
+//
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	let myVar = 10;
+
+	return (
+		<View style={styles.container}>
+			<Text>My amazing app! {myVar}</Text>
+			<Text style={{ color: "blue" }}>Some more text</Text>
+			<GiveData />
+			<Button title="Click Me!" />
+
+			<StatusBar style="auto" />
+		</View>
+	);
+}
+
+function GiveData() {
+	return <Text>Here's some data</Text>;
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: "#fff",
+		alignItems: "center",
+		justifyContent: "center",
+	},
 });
