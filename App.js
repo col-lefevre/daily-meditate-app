e356da266cf943085fa03e467cfe4a5ca77eb265
Colin LeFevre <colin.lefevre.school@gmail.com>
2023-09-18 20:30:08 -0400

    w5a1

--- App.js
+++ App.js
@@ -1,4 +1,5 @@
 import { StyleSheet, Text, View, Button } from "react-native";
+import { StatusBar } from "expo-status-bar";
 import { gradebook } from "./data.js";
 
 export default function App() {
@@ -24,11 +25,14 @@ export default function App() {
 
 			<View style={{ paddingTop: 20 }}>
 				{gradebook.map((student) => (
-					<Text style={{ paddingTop: 10, color: "blue" }}>
-						Name: {student.name} | Grade: {student.grade}
-					</Text>
+					<View>
+						<Text style={{ paddingTop: 10, color: "blue" }}>
+							Name: {student.name} | Grade: {student.grade}%
+						</Text>
+					</View>
 				))}
 			</View>
+			<StatusBar style="auto" />
 		</View>
 	);
 }
